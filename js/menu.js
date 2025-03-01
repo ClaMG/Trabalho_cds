9393d7b4bc985bcbf0cd3ddb635a704c455678b8
Cla <157390340+ClaMG@users.noreply.github.com>
2025-03-01 18:36:04 -0300

    vsf eu tento amanha

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,21 @@
 function aparecer_g(el){
-    if(document.getElementById(el).style.display === 'flex'){
-        document.getElementById(el).style.display = 'none'
+    if(document.getElementById('sumir_g').style.display === 'flex'){
+        document.getElementById('sumir_g').style.display = 'none'
     } else{
-        document.getElementById(el).style.display = 'flex'
+        document.getElementById('sumir_g').style.display = 'flex'
     }
 }
     
 function aparecer_a(el){
-    if(document.getElementById(el).style.display === 'flex'){
-        document.getElementById(el).style.display = 'none'
+    if(document.getElementById('sumir_a').style.display === 'flex'){
+        document.getElementById('sumir_a').style.display = 'none'
     } else{
-        document.getElementById(el).style.display = 'flex'
+        document.getElementById('sumir_a').style.display = 'flex'
     }
 }
     
+if(document.getElementById('sumir_g').style.display === 'flex'){
+    document.getElementById('sumir_a').style.display = 'none'
+}else if(document.getElementById('sumir_a').style.display === 'flex'){
+    document.getElementById('sumir_g').style.display = 'none'
+}
