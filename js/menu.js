013179fd525b13f304ff1ad9ddf030f9657ddf24
Cla <157390340+ClaMG@users.noreply.github.com>
2025-03-01 17:35:22 -0300

    vsf

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,12 @@
-function aparecer(el){
+function aparecer_g(el){
+    if(document.getElementById(el).style.display === 'flex'){
+        document.getElementById(el).style.display = 'none'
+    } else{
+        document.getElementById(el).style.display = 'flex'
+    }
+}
+    
+function aparecer_a(el){
     if(document.getElementById(el).style.display === 'flex'){
         document.getElementById(el).style.display = 'none'
     } else{
