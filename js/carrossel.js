a64ade04b26c8b0c5ecfc71ba7b1ee07c7f832f9
Cla <157390340+ClaMG@users.noreply.github.com>
2025-03-02 13:44:11 -0300

    cansei ja

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,26 +1,18 @@
-// Seleciona o primeiro botão de navegação manual (caso precise ser manipulado)
-var radio = document.querySelector('.manual_btn');
+var radio = document.querySelector('.manual_btn')
+var cont = 1
 
-// Contador para controlar qual slide está ativo
-var cont = 1;
+document.getElementById('radio1').checked = true
 
-// Inicializa o primeiro slide (radio1) como sendo o ativo
-document.getElementById('radio1').checked = true;
-
-// Configura um intervalo para mudar a imagem automaticamente a cada 5 segundos
 setInterval(() => {
-    proximaImg(); // Chama a função para mudar para o próximo slide
-}, 5000);
+    proximaImg()
+}, 5000)
 
-// Função para mudar para o próximo slide
 function proximaImg(){
-    cont++; // Incrementa o contador para passar para o próximo slide
+    cont++
 
-    // Se o contador ultrapassar 3 (o número máximo de slides), ele é resetado para 1
     if(cont > 3){
-        cont = 1;
+        cont = 1 
     }
 
-    // Marca o botão de rádio correspondente ao slide atual como "checked" (ativo)
-    document.getElementById('radio' + cont).checked = true;
+    document.getElementById('radio'+cont).checked = true
 }
