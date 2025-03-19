e682797939fa32c1533790228fdbf59d4d3270eb
Cla <157390340+ClaMG@users.noreply.github.com>
2025-03-19 13:50:38 -0300

    correção de mascara

--- js/cadastro/mascara_tel.js
+++ js/cadastro/mascara_tel.js
@@ -9,9 +9,9 @@ document.getElementById('telefone').addEventListener('input', function(e) {
     if (telefone.length <= 2) {
         telefone = telefone.replace(/(\d{1,2})/, '($1)');
     } else if (telefone.length <= 6) {
-        telefone = telefone.replace(/(\d{2})(\d{1,4})/, '($1) $2');
+        telefone = telefone.replace(/(\d{2})(\d{1,5})/, '($1) $2');
     } else {
-        telefone = telefone.replace(/(\d{2})(\d{1,4})(\d{1,4})/, '($1) $2-$3');
+        telefone = telefone.replace(/(\d{2})(\d{1,5})(\d{1,4})/, '($1) $2-$3');
     }
 
     // Atualiza o valor do campo de telefone
