1a6b5367e5c5f3720e1d2436e6b5831b447b6a0a
Cla <157390340+ClaMG@users.noreply.github.com>
2025-03-19 09:59:44 -0300

    organização

--- js/Login/login.js
+++ js/Login/login.js
@@ -1,12 +1,22 @@
 function togglePassword() {
+    // Obtém a referência ao campo de entrada de senha
     let passwordInput = document.getElementById("password");
+    
+    // Obtém a referência ao ícone do olho
     let eyeIcon = document.getElementById("img_olho");
 
+    // Verifica se o tipo do campo de senha é "password"
     if (passwordInput.type === "password") {
+        // Altera o tipo para "text" para exibir a senha
         passwordInput.type = "text";
-        eyeIcon.src = "../img/icones/icone_olho_aberto.png"; // Troca o ícone para olho aberto
+        
+        // Altera a imagem do ícone para um olho aberto
+        eyeIcon.src = "../img/login/icone_olho_aberto.png";
     } else {
+        // Retorna o tipo do campo para "password" para esconder a senha
         passwordInput.type = "password";
-        eyeIcon.src = "../img/icones/icone_olho_fechado.png"; // Volta para o ícone de olho fechado
+        
+        // Altera a imagem do ícone para um olho fechado
+        eyeIcon.src = "../img/login/icone_olho_fechado.png";
     }
 }
